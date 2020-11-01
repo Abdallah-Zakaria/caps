14579107c1c72277826a1ef58aaad85ccade400e
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-11-01 22:27:28 +0200

    class-16 step1

---
+++ vendor.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const events = require('./events');
+const faker = require('faker');
+require('dotenv').config();
+
+function intervalFunc() {
+  const obj = {
+    storeName: process.env.STORE_NAME || 'storeName',
+    orderId: faker.random.uuid(),
+    customerName: faker.name.findName(),
+    address: faker.address.city()
+  }
+  events.emit('pickup', obj);
+}
+setInterval(intervalFunc, 5000);
+
+events.on('delivered', (payload) => {
+  console.log(`VENDOR : Thank you for delivering ${payload.orderId}`);
+});
